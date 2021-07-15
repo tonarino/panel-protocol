d9e7760bce842f513add27cf67b952d0da04c32e
Edward Yang <edward.yang6771@gmail.com>
2021-07-14 20:08:14 -0500

    Feature parity with all commands, collapsing

--- examples/gui/app.rs
+++ examples/gui/app.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use eframe::{
-    egui::{self, FontDefinitions, FontFamily, Vec2},
+    egui::{self, FontDefinitions, FontFamily, ScrollArea, Vec2},
     epi::{self, Storage},
 };
 use panel_protocol::{Command, PulseMode, Report};
@@ -44,10 +44,18 @@ impl From<LedState> for Command {
         }
     }
 }
+
+#[derive(Default, Clone, Copy, PartialEq)]
+struct LightState {
+    brightness: u16,
+    temperature: u16,
+}
+
 pub struct App {
     report_rx: Receiver<Report>,
     command_tx: Sender<Command>,
     led_state: LedState,
+    light_state: [LightState; 2],
     last_recv_reports: VecDeque<Report>,
     kill_updater: Option<Sender<()>>,
 }
@@ -58,9 +66,106 @@ impl App {
             report_rx,
             command_tx,
             led_state: Default::default(),
+            light_state: Default::default(),
             last_recv_reports: VecDeque::new(),
             kill_updater: None,
         }
+    }
+
+    fn led_configuration_section(&mut self, ui: &mut eframe::egui::Ui) {
+        ui.add(
+            egui::Slider::new(&mut self.led_state.r, 0..=255).text("LED Red").clamp_to_range(true),
+        );
+        ui.add(
+            egui::Slider::new(&mut self.led_state.g, 0..=255)
+                .text("LED Green")
+                .clamp_to_range(true),
+        );
+        ui.add(
+            egui::Slider::new(&mut self.led_state.b, 0..=255).text("LED Blue").clamp_to_range(true),
+        );
+
+        // Pulse mode
+        egui::ComboBox::from_label("Pulse Mode")
+            .selected_text(format!("{:?}", self.led_state.pulse_mode))
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut self.led_state.pulse_mode, PulseMode::Solid, "Solid");
+                ui.selectable_value(
+                    &mut self.led_state.pulse_mode,
+                    PulseMode::DialTurn,
+                    "DialTurn",
+                );
+                ui.selectable_value(
+                    &mut self.led_state.pulse_mode,
+                    PulseMode::Breathing {
+                        interval_ms: NonZeroU16::new(self.led_state.if_breathing_interval_ms)
+                            .unwrap(),
+                    },
+                    "Breathing",
+                );
+            });
+
+        // Duration for pulse mode if breathing
+        ui.scope(|ui| {
+            ui.set_visible(matches!(self.led_state.pulse_mode, PulseMode::Breathing { .. }));
+
+            let response = ui.add(
+                egui::Slider::new(&mut self.led_state.if_breathing_interval_ms, 1..=u16::MAX)
+                    .text("Breathing (half) interval (ms)")
+                    .clamp_to_range(true),
+            );
+            if response.changed() {
+                self.led_state.pulse_mode = PulseMode::Breathing {
+                    interval_ms: NonZeroU16::new(self.led_state.if_breathing_interval_ms).unwrap(),
+                };
+            }
+        });
+    }
+
+    fn lighting_configuration_section(&mut self, ui: &mut eframe::egui::Ui) {
+        ui.label("Front Lights");
+        ui.group(|ui| {
+            ui.add(
+                egui::Slider::new(&mut self.light_state[0].brightness, 0..=u16::MAX)
+                    .text("Brightness")
+                    .clamp_to_range(true),
+            );
+            ui.add(
+                egui::Slider::new(&mut self.light_state[0].temperature, 0..=u16::MAX)
+                    .text("Temperature")
+                    .clamp_to_range(true),
+            );
+        });
+        ui.label("Back Lights");
+        ui.group(|ui| {
+            ui.add(
+                egui::Slider::new(&mut self.light_state[1].brightness, 0..=u16::MAX)
+                    .text("Brightness")
+                    .clamp_to_range(true),
+            );
+            ui.add(
+                egui::Slider::new(&mut self.light_state[1].temperature, 0..=u16::MAX)
+                    .text("Temperature")
+                    .clamp_to_range(true),
+            );
+        });
+    }
+
+    fn other_commands_section(&mut self, ui: &mut eframe::egui::Ui) {
+        if ui.button(format!("Send {:?} command", Command::Bootload)).clicked() {
+            self.command_tx.send(Command::Bootload).unwrap();
+        }
+    }
+
+    fn serial_monitor_section(&mut self, ui: &mut eframe::egui::Ui) {
+        ui.group(|ui| {
+            let commands_strings = self
+                .last_recv_reports
+                .iter()
+                .map(|report| format!("New serial message received: {:?}", report))
+                .collect::<Vec<_>>();
+            ui.add(egui::Label::new(commands_strings.join("\n")).code())
+        });
     }
 }
 
@@ -103,6 +208,7 @@ impl epi::App for App {
 
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
         let current_led_state = self.led_state.clone();
+        let current_light_state = self.light_state.clone();
         egui::CentralPanel::default().show(ctx, |ui| {
             if let Ok(report) = self.report_rx.try_recv() {
                 self.last_recv_reports.push_back(report);
@@ -111,95 +217,51 @@ impl epi::App for App {
                 }
             }
 
-            ui.spacing_mut().slider_width = ui.available_width() - 400.0;
-            ui.spacing_mut().item_spacing = Vec2::new(10.0, 10.0);
-            ui.spacing_mut().button_padding = Vec2::new(10.0, 10.0);
-            ui.vertical_centered_justified(|ui| {
-                ui.heading("Panel Configurator");
-                // RGB sliders
-                ui.add(
-                    egui::Slider::new(&mut self.led_state.r, 0..=255)
-                        .text("LED Red")
-                        .clamp_to_range(true),
-                );
-                ui.add(
-                    egui::Slider::new(&mut self.led_state.g, 0..=255)
-                        .text("LED Green")
-                        .clamp_to_range(true),
-                );
-                ui.add(
-                    egui::Slider::new(&mut self.led_state.b, 0..=255)
-                        .text("LED Blue")
-                        .clamp_to_range(true),
-                );
+            ScrollArea::auto_sized().show(ui, |ui| {
+                ui.spacing_mut().slider_width = ui.available_width() - 300.0;
+                ui.spacing_mut().item_spacing = Vec2::new(10.0, 10.0);
+                ui.spacing_mut().button_padding = Vec2::new(10.0, 10.0);
+                ui.vertical_centered_justified(|ui| {
+                    ui.heading("Panel Configurator");
+                    // RGB sliders
+                    ui.separator();
+                    ui.collapsing("RGB LED Configuration", |ui| self.led_configuration_section(ui));
 
-                // Pulse mode
-                egui::ComboBox::from_label("Pulse Mode")
-                    .selected_text(format!("{:?}", self.led_state.pulse_mode))
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut self.led_state.pulse_mode,
-                            PulseMode::Solid,
-                            "Solid",
-                        );
-                        ui.selectable_value(
-                            &mut self.led_state.pulse_mode,
-                            PulseMode::DialTurn,
-                            "DialTurn",
-                        );
-                        ui.selectable_value(
-                            &mut self.led_state.pulse_mode,
-                            PulseMode::Breathing {
-                                interval_ms: NonZeroU16::new(
-                                    self.led_state.if_breathing_interval_ms,
-                                )
-                                .unwrap(),
-                            },
-                            "Breathing",
-                        );
-                    });
+                    // Lighting
+                    ui.separator();
+                    ui.collapsing("Lighting", |ui| self.lighting_configuration_section(ui));
 
-                // Duration for pulse mode if breathing
-                ui.scope(|ui| {
-                    ui.set_visible(matches!(
-                        self.led_state.pulse_mode,
-                        PulseMode::Breathing { .. }
-                    ));
+                    // Booloader command
+                    ui.separator();
+                    ui.collapsing("Other Commands", |ui| self.other_commands_section(ui));
 
-                    let response = ui.add(
-                        egui::Slider::new(
-                            &mut self.led_state.if_breathing_interval_ms,
-                            1..=u16::MAX,
-                        )
-                        .text("Breathing (half) interval (ms)")
-                        .clamp_to_range(true),
+                    // Show last few commands
+                    ui.separator();
+                    ui.collapsing(
+                        format!("Serial Monitor (last {} messages)", SHOW_LAST_COMMAND_NUM),
+                        |ui| self.serial_monitor_section(ui),
                     );
-                    if response.changed() {
-                        self.led_state.pulse_mode = PulseMode::Breathing {
-                            interval_ms: NonZeroU16::new(self.led_state.if_breathing_interval_ms)
-                                .unwrap(),
-                        };
-                    }
-                });
 
-                // Show last few commands
-                ui.heading(format!("Serial Monitor (last {} messages)", SHOW_LAST_COMMAND_NUM));
-                ui.group(|ui| {
-                    let commands_strings = self
-                        .last_recv_reports
-                        .iter()
-                        .map(|report| format!("New serial message received: {:?}", report))
-                        .collect::<Vec<_>>();
-                    ui.add(egui::Label::new(commands_strings.join("\n")).code())
+                    // Warn if debug build
+                    egui::warn_if_debug_build(ui);
                 });
-
-                // Warn if debug build
-                egui::warn_if_debug_build(ui);
             });
         });
 
         if self.led_state != current_led_state {
             self.command_tx.send(self.led_state.into()).unwrap();
+        }
+
+        if self.light_state != current_light_state {
+            for (target, state) in self.light_state.iter().enumerate() {
+                let target = target as u8;
+                self.command_tx
+                    .send(Command::Brightness { target, value: state.brightness })
+                    .unwrap();
+                self.command_tx
+                    .send(Command::Temperature { target, value: state.temperature })
+                    .unwrap();
+            }
         }
     }
 
